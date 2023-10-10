cbb1fd5b620286a37b83f5909151df6ecb8f6412
Vuong Quoc Nguyen <nguvuong@sheridancollege.ca>
2023-10-10 17:54:24 -0400

    finish the course

---
+++ 02_vars/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var age int32 = 37
+	var isCool = false
+
+	// Shorthand
+	name := "Vuong"
+	size := 1.3
+
+	fmt.Println(name, age, isCool)
+	fmt.Printf("%T\n", size)
+}
